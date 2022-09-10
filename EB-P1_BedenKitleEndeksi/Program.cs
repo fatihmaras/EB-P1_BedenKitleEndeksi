c5342197cfa0ba6238d2c55b200d5eb566a3ca0d
fatihmaras <fatihmmaras@gmail.com>
2022-09-11 02:45:48 +0300

    Kodlar Eklendi

---
+++ EB-P1_BedenKitleEndeksi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_BedenKitleEndeksi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kilonuzu Giriniz");
+            int kilo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Boyunuzu Giriniz");
+            double Boy = Convert.ToDouble(Console.ReadLine());
+
+            double bke = kilo / (Boy * Boy);
+
+            if (bke < 18)
+            {
+                Console.WriteLine("Zayıfsınız...");
+                Console.ReadLine();
+
+            }
+            else if (bke >= 18 && bke < 25)
+            {
+
+
+                Console.WriteLine("Normalsiniz");
+                Console.ReadLine();
+            }
+            else if (bke >= 25)
+            {
+                Console.WriteLine("Obezsiniz");
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
